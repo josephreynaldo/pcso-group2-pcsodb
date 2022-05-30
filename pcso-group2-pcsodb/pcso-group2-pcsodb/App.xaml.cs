d9d898e0324a9f9210c18187774e036bddc2a17d
Joseph Reynaldo <joseph.reynaldo@pcso.gov.ph>
2022-05-30 10:01:21 +0800

    test

---
+++ pcso-group2-pcsodb/pcso-group2-pcsodb/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace pcso_group2_pcsodb;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ pcso-group2-pcsodb/pcso-group2-pcsodb/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace pcso_group2_pcsodb;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
