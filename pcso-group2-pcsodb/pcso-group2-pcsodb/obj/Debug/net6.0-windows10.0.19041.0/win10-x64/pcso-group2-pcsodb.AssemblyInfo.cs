d97229d1aea07b7bdaed3dda90bce70cdf69e66a
Joseph Reynaldo <joseph.reynaldo@pcso.gov.ph>
2022-05-30 12:24:40 +0800

    test

--- pcso-group2-pcsodb/pcso-group2-pcsodb/obj/Debug/net6.0-windows10.0.19041.0/win10-x64/pcso-group2-pcsodb.AssemblyInfo.cs
+++ pcso-group2-pcsodb/pcso-group2-pcsodb/obj/Debug/net6.0-windows10.0.19041.0/win10-x64/pcso-group2-pcsodb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pcso-group2-pcsodb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("pcso-group2-pcsodb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pcso-group2-pcsodb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
